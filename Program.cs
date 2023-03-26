22768b27103f7011348a7299eccdfc27b3757cc6
André Nascimento <andre4.20nascimento@outlook.com>
2023-03-26 20:43:27 -0300

    Executável

---
+++ Program.cs
@@ -0,0 +1,22 @@
+﻿Console.Clear();
+
+Console.WriteLine("--- Decomposição Decimal ---");
+
+Console.Write("\nDigite um número inteiro...: ");
+int numero = Convert.ToInt32(Console.ReadLine());
+
+int restante = numero; 
+
+int unidades = restante % 10; // Pega o numero digitado e mostra apenas o resto da Divisao por 10.
+restante /= 10;
+
+int desenas = restante % 10;
+restante /= 10;
+
+int centenas = restante;
+
+Console.WriteLine($"\nO número {numero} possui:");
+Console.WriteLine($"{unidades,10} unidades(s)");
+Console.WriteLine($"{desenas,10} desena(s)");
+Console.WriteLine($"{centenas,10} centena(s)");
+
